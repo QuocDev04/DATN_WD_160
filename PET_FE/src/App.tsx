018a26375fff1fb5dd4a5e6281499db8800eb122
quoc <huyquoc2xx4@gmail.com>
2024-09-26 16:51:30 +0700

    BASE FE

---
+++ PET_FE/src/App.tsx
@@ -0,0 +1,6 @@
+
+
+// B1: npm i antd @ant-design/cssinjs
+// B2: truy cập : https://ant.design/docs/react/compatible-style#tailwindcss-arrange-layer
+// copy css vào global.css
+// B3: Sử dụng component
